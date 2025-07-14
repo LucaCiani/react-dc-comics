b675ac50f40fd08fb55a4e196ac6751cc6c586b7
LucaCiani <lucaciani88@gmail.com>
2025-07-14 18:38:27 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
 function App() {
-    return <></>;
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <div className="header-flex">
+                        <div>
+                            <img
+                                className="logo"
+                                src="/dc-logo.png"
+                                alt="logo"
+                            />
+                        </div>
+                        <div>
+                            <nav className="header-nav">
+                                <span href="#">CHARACTERS</span>
+                                <span className="comics" href="#">
+                                    COMICS
+                                </span>
+                                <span href="#">MOVIES</span>
+                                <span href="#">TV</span>
+                                <span href="#">GAMES</span>
+                                <span href="#">COLLECTIBLES</span>
+                                <span href="#">VIDEOS</span>
+                                <span href="#">FANS</span>
+                                <span href="#">NEWS</span>
+                                <span href="#">SHOP</span>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div className="container">
+                    <div className="content"> next content</div>
+                    <div className="shop"></div>
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default App;
